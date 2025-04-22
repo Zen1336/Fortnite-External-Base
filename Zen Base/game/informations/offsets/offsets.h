a00a4c17c2a632d5410110126b56b664962eb9d6
Zen1336 <fnaccgglok@gmail.com>
2025-04-22 17:35:06 -0500

    Update offsets

--- Zen Base/game/informations/offsets/offsets.h
+++ Zen Base/game/informations/offsets/offsets.h
@@ -3,15 +3,15 @@
 
 namespace OFFSETS
 {
-    uintptr_t UWorld = 0x16DA8288;
-    uintptr_t GameInstance = 0x210;
+    uintptr_t UWorld = 0x1782D0B8;
+    uintptr_t GameInstance = 0x220;
     uintptr_t PawnPrivate = 0x320;
     uintptr_t LocalPlayers = 0x38;
     uintptr_t PlayerController = 0x30;
     uintptr_t LocalPawn = 0x350;
     uintptr_t PlayerState = 0x2C8;
     uintptr_t RootComponent = 0x1B0;
-    uintptr_t GameState = 0x198;
+    uintptr_t GameState = 0x1A8;
     uintptr_t Cameramanager = 0x360;
     uintptr_t Mesh = 0x328;
     uintptr_t TeamId = 0x1259;
